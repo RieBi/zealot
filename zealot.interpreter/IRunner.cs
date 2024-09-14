c8b3412a3b95a1548930488c4d6a680eb6d47ce8
RieBi <42877671+RieBi@users.noreply.github.com>
2024-09-14 20:01:24 +0200

    Create public members for interpreter

---
+++ zealot.interpreter/IRunner.cs
@@ -0,0 +1,9 @@
+﻿namespace zealot.interpreter;
+public interface IRunner
+{
+    /// <summary>
+    /// Ask the runner to get the next line of code, without which the interpreter cannot continue execution.
+    /// </summary>
+    /// <returns></returns>
+    string GetNextLine();
+}
---
+++ zealot.interpreter/Interpreter.cs
@@ -0,0 +1,16 @@
+﻿namespace zealot.interpreter;
+public class Interpreter(IRunner runner)
+{
+	private IRunner _runner = runner;
+
+    /// <summary>
+    /// Interprets the <paramref name="line"/> of code and returns returned result, if any.
+    /// </summary>
+    /// <param name="line"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public string? InterpretLine(string line)
+	{
+        return line;
+	}
+}
