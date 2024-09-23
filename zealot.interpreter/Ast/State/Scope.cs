6d1fcb567e11f22d16e9121820baccdb5a314ce0
RieBi <42877671+RieBi@users.noreply.github.com>
2024-09-23 18:53:23 +0200

    Add scope methods for manipulating variables

--- zealot.interpreter/Ast/State/Scope.cs
+++ zealot.interpreter/Ast/State/Scope.cs
@@ -1,5 +1,48 @@
-﻿namespace Zealot.Interpreter.Ast.State;
+﻿using System.Diagnostics.CodeAnalysis;
+using Zealot.Interpreter.Ast.Types;
+
+namespace Zealot.Interpreter.Ast.State;
 internal class Scope
 {
     public Scope? ParentScope { get; set; }
+
+    private readonly Dictionary<string, TypeInfo> _variables = [];
+
+    public void DefineVariable(string identifier, TypeInfo value, bool replaceExisting = false)
+    {
+        if (!replaceExisting && HasVariable(identifier))
+            throw new InvalidOperationException($"Cannot define a variable {identifier}: it is already defined.");
+
+        _variables[identifier] = value;
+    }
+
+    /// <summary>
+    /// Returns whether the scope has a variable named <paramref name="identifier"/> defined in it, excluding parent scopes.
+    /// </summary>
+    /// <param name="identifier"></param>
+    /// <returns></returns>
+    public bool HasOwnVariable(string identifier) => _variables.ContainsKey(identifier);
+
+    /// <summary>
+    /// Returns whether the scope or any of its parent scopes has a variable named <paramref name="identifier"/> defined in it.
+    /// </summary>
+    /// <param name="identifier"></param>
+    /// <returns></returns>
+    public bool HasVariable(string identifier) => HasOwnVariable(identifier) || ParentScope?.HasVariable(identifier) == true;
+
+    /// <summary>
+    /// Gets the value of a variable named <paramref name="identifier"/> defined in the scope, excluding parent scopes.
+    /// </summary>
+    /// <param name="identifier"></param>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public bool TryGetOwnVariable(string identifier, [MaybeNullWhen(false)] out TypeInfo value) => _variables.TryGetValue(identifier, out value);
+
+    /// <summary>
+    /// Gets the value of a variable named <paramref name="identifier"/> in the scope or any of its parent scopes.
+    /// </summary>
+    /// <param name="identifier"></param>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public bool TryGetVariable(string identifier, [MaybeNullWhen(false)] out TypeInfo value) => TryGetOwnVariable(identifier, out value) || ParentScope?.TryGetVariable(identifier, out value) == true;
 }
